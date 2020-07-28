7dade2d5d3f535583107baae585ad77a673c4b5b
William McDonald <ginoskotheon@yahoo.com>
2020-07-28 16:38:19 -0500

    added a button shine

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let theme = localStorage.getItem('theme');
+const cards = document.querySelectorAll('.box');
+const button = document.querySelector('.button');
+
+const themeButs = document.querySelectorAll('.theme');
 
 if (theme == null) {
   setTheme('dark');
@@ -29,3 +33,19 @@ function setTheme(mode) {
 
   localStorage.setItem('theme', mode);
 }
+
+button.addEventListener('mouseover', function (e) {
+  let x = e.clientX - e.target.offsetLeft;
+  let y = e.clientY - e.target.offsetTop;
+
+  let ripples = document.createElement('span');
+  ripples.className = 'ripple';
+
+  ripples.style.left = x + 'px';
+  ripples.style.top = y + 'px';
+  this.appendChild(ripples);
+
+  setTimeout(() => {
+    ripples.remove();
+  }, 500);
+});
